fe87d14be8c59c9b08b4fa393207b68411fc08a0
Jorge Padilla <jorge@priego8a.com>
2020-03-20 00:12:26 -0600

    posicion

---
+++ Practicas/firebase/js/basededatos.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCdoVGfHrjNgcQTmWf9oU2uuxY3MLp3mB0",
+    authDomain: "sistemasgeoreferenciados66176.firebaseapp.com",
+    databaseURL: "https://sistemasgeoreferenciados66176.firebaseio.com",
+    projectId: "sistemasgeoreferenciados66176",
+    storageBucket: "sistemasgeoreferenciados66176.appspot.com",
+    messagingSenderId: "81175952642",
+    appId: "1:81175952642:web:508ea16f17e1dfa12d961b"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
